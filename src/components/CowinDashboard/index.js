1b646bf69584df7799861c1f574ca4166d130a92
chandrika <balimidivenkatachandrika@gmail.com>
2023-06-13 07:56:04 +0000

    cowin

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -88,38 +88,36 @@ class CoWinDashboard extends Component {
       <>
         <VaccinationCoverage>
           <h1 className="heading">Vaccination Coverage</h1>
-          <ResponsiveContainer width="100%" height={500}>
-            <BarChart
-              width={1000}
-              height={300}
-              margin={{
-                top: 5,
+          <BarChart
+            width={1000}
+            height={300}
+            margin={{
+              top: 5,
+            }}
+            data={vaccinationData}
+          >
+            <XAxis
+              dataKey="vaccineDate"
+              tick={{
+                stroke: 'gray',
+                strokeWidth: 1,
               }}
-              data={vaccinationData}
-            >
-              <XAxis
-                dataKey="vaccineDate"
-                tick={{
-                  stroke: 'gray',
-                  strokeWidth: 1,
-                }}
-              />
-              <YAxis
-                tickFormatter={DataFormatter}
-                tick={{
-                  stroke: 'gray',
-                  strokeWidth: 0,
-                }}
-              />
-              <Legend
-                wrapperStyle={{
-                  padding: 30,
-                }}
-              />
-              <Bar dataKey="dose1" name="Dose 1" fill="#5a8dee" barSize="20%" />
-              <Bar dataKey="dose2" name="Dose 2" fill="#f54394" barSize="20%" />
-            </BarChart>
-          </ResponsiveContainer>
+            />
+            <YAxis
+              tickFormatter={DataFormatter}
+              tick={{
+                stroke: 'gray',
+                strokeWidth: 0,
+              }}
+            />
+            <Legend
+              wrapperStyle={{
+                padding: 30,
+              }}
+            />
+            <Bar dataKey="dose1" name="Dose 1" fill="#5a8dee" barSize="20%" />
+            <Bar dataKey="dose2" name="Dose 2" fill="#f54394" barSize="20%" />
+          </BarChart>
         </VaccinationCoverage>
         <VaccinationByGender>
           <h1 className="heading">Vaccination by gender</h1>
@@ -150,30 +148,28 @@ class CoWinDashboard extends Component {
         </VaccinationByGender>
         <VaccinationByAge>
           <h1 className="heading">Vaccination by Age</h1>
-          <ResponsiveContainer width="100%" height={300}>
-            <PieChart>
-              <Pie
-                cx="70%"
-                cy="40%"
-                data={vaccinationByAge}
-                startAngle={0}
-                endAngle={360}
-                innerRadius="0%"
-                outerRadius="70%"
-                dataKey="count"
-              >
-                <Cell name="18-44" fill=" #2d87bb" />
-                <Cell name="45-60" fill="#a3df9f" />
-                <Cell name="Above 60" fill=" #64c2a6" />
-              </Pie>
-              <Legend
-                iconType="circle"
-                layout="horizontal"
-                verticalAlign="bottom"
-                align="center"
-              />
-            </PieChart>
-          </ResponsiveContainer>
+          <PieChart>
+            <Pie
+              cx="70%"
+              cy="40%"
+              data={vaccinationByAge}
+              startAngle={0}
+              endAngle={360}
+              innerRadius="0%"
+              outerRadius="70%"
+              dataKey="count"
+            >
+              <Cell name="18-44" fill=" #2d87bb" />
+              <Cell name="45-60" fill="#a3df9f" />
+              <Cell name="Above 60" fill=" #64c2a6" />
+            </Pie>
+            <Legend
+              iconType="circle"
+              layout="horizontal"
+              verticalAlign="bottom"
+              align="center"
+            />
+          </PieChart>
         </VaccinationByAge>
       </>
     )
